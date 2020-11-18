1d1dfe554d8c12a14bd521d681798ee8fd85df41
Eddges <shivamshekhar.i7@gmail.com>
2020-11-18 14:13:52 +0530

    Navbar links and animations added

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -2,6 +2,7 @@ import React from 'react'
 import classes from './Navbar.module.css'
 import Logo from '../../assets/Logo.svg'
 import {NavLink} from 'react-router-dom'
+import './Navbar.css'
 
 class Navbar extends React.Component{
 
@@ -9,19 +10,32 @@ class Navbar extends React.Component{
         return(
             <div className={classes.Container}>
 
-                <div className={classes.Logo}>
+                <NavLink to='/' exact className={classes.Logo}>
                     <img src={Logo} alt="Logo" />
-                </div>
+                </NavLink>
                 
-                <NavLink to="/" exact className={classes.Menu}>
-                    <div className={classes.MenuItem}>
+                <div className={classes.Menu} >
+
+                    <NavLink to="/doctors" exact className={classes.MenuItem} activeClassName='ActiveMenuItem' >
                         <span className={classes.MenuText}>Doctors</span>
                         <span className={classes.ActiveMenuItem}></span>
-                    </div>
-                </NavLink>
+                    </NavLink>
+
+                    <NavLink to="/consult" exact className={classes.MenuItem} activeClassName='ActiveMenuItem' >
+                        <span className={classes.MenuText}>Consult</span>
+                        <span className={classes.ActiveMenuItem}></span>
+                    </NavLink>
+
+                    <NavLink to="/professional" exact className={classes.MenuItem} activeClassName='ActiveMenuItem' >
+                        <span className={classes.MenuText}>For Doctors &nbsp; &#x25BC;</span>
+                        <span className={classes.ActiveMenuItem}></span>
+                    </NavLink>
+
+                </div>
 
                 <div className={classes.ButtonDiv}>
-
+                    <NavLink to='/login' className={classes.LoginButton}>Login</NavLink>
+                    <NavLink to='/signup' className={classes.SignUpButton}>Sign Up</NavLink>
                 </div>
 
             </div>
